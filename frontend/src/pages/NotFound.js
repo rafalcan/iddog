b09f525c0aa5cc764ebacadc736eb1cbfe0766ac
Rafael Gomes Alcantara <rafalcan@gmail.com>
2019-07-23 05:32:39 -0300

    include style in the NotFound page

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Container } from 'reactstrap';
+import './NotFound.scss';
 
 const NotFound = () => (
-  <h2>Page not found</h2>
+  <Container className="notfound">
+    <div className="content">
+      <h2>Página não encontrada :-(</h2>
+    </div>
+  </Container>
 );
 
 export default NotFound;
